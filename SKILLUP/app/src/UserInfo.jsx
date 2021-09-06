d9dc02df5ec7848a4b069460072a50618fbd0b84
Teodora Postovan <postovan.teodora@gmail.com>
2021-09-06 22:58:28 +0300

    add salary and horizontal line

--- SKILLUP/app/src/UserInfo.jsx
+++ SKILLUP/app/src/UserInfo.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+
 function UserInfo(){
+
     return(
         <div>
             <UserDatas userName="Ian" userGender="M" userAge="32" yearsWorked="10" workPlace="Microsoft" />
@@ -10,13 +12,28 @@ function UserInfo(){
     )
 }
 
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 2,
+            width: 300,
+            margin: 0
+        }}
+    />
+);
 function UserDatas(props) {
 return(
     <div>
+        <div style={{margin:10}}>
         <h4>Name: {props.userName}</h4>
         <p>Gender: {props.userGender}</p>
         <p>Age: {props.userAge}</p>
-        <p>Years of working: {props.yearsWorked} at: {props.workPlace}</p>
+        <p>Years of working: {props.yearsWorked} at {props.workPlace}</p>
+        <p>Salary: {props.yearsWorked * 10}</p>
+        <ColoredLine color="grey" />
+        </div>
     </div>
 )
 }
